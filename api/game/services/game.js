7e83831b5616d9bc191d02086b42dfcace919c9b
Thiago Marinho <tgmarinho@gmail.com>
2021-08-25 06:40:45 -0400

    starting populate data publisher and developer

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,8 +1,53 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const axios = require("axios");
+const slugify = require("slugify");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = axios.get(`https://www.gog.com/game/{$slug}`);
+  const dom = new JSDOM(body.data);
+  const description = dom.window.document.querySelector(".description");
+  return {
+    rating: "FREE",
+    short_description: description.texContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true }),
+    });
+  }
+}
+
+module.exports = {
+  populate: async (params) => {
+    const gogApiUrl =
+      "https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity";
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(products[0]);
+
+    await create(products[0].publisher, "publisher");
+    await create(products[0].developer, "developer");
+  },
+};
