a603d155dd4c119caf773676b8145a116971cba8
Thiago Marinho <tgmarinho@gmail.com>
2021-10-11 06:44:27 -0400

    add query params to service

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -8,8 +8,17 @@
 // curl -X POST http://localhost:1337/games/populate\?page\=1\&search\=bla
 module.exports = {
   populate: async (ctx) => {
-    console.log("Starting to populate");
-    await strapi.services.game.populate();
-    ctx.send("Finishing populate");
+    console.log("Starting to populate...");
+
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options);
+
+    ctx.send("Finished populating!");
+  },
   },
 };
